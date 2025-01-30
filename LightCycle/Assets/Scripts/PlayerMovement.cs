460766613f0037457415148e5d790539354219a0
Bubo4400 <zachturtle4@gmail.com>
2025-01-30 17:02:34 +0100

    Jump and gravity addition

--- LightCycle/Assets/Scripts/PlayerMovement.cs
+++ LightCycle/Assets/Scripts/PlayerMovement.cs
@@ -4,9 +4,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // Le controlleur pour le jour
+    public CharacterController player;
+
     public float speed = 5.0f; // Vitesse de déplacement
+    private float gravity = -19.62f; // Gravite pour tomber
+    private Vector3 velocity; // Vitesse de chute
+    private float jumpHeight = 2f; // Puissance du saut
+
 
     private TrailRenderer trailRenderer;
+    public Transform isGrounded; // si le joueur touche le sol
+    public LayerMask ground;
+    private bool grounded = false ;
 
     void Start()
     {
@@ -20,12 +30,31 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        playerMovement();
+    }
+
+    private void playerMovement()
+    {
+        // Verifier si le joueur et sur le sol
+        grounded = Physics.CheckSphere(isGrounded.position, 1f, ground);
+
+        // Si le joueur et sur le sol arreter le velocity d'augmenter
+        if (grounded && velocity.y < 0)
+            velocity.y = -2f;
+
+        if (Input.GetButtonDown("Jump") && grounded)
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+
+        // Application du gravite
+        velocity.y += gravity * Time.deltaTime;
+        player.Move(velocity * Time.deltaTime);
+
         // Déplacement avec les flèches directionnelles
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
         // Mouvement dans le plan XZ
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical).normalized;
+        player.Move(movement * speed * Time.deltaTime);
     }
 }
