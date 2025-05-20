250c8c5283ad1afbe4a51d4ffe69d501bd0a9630
Wanous <163208990+Wanous@users.noreply.github.com>
2025-05-20 02:41:29 +0200

    modification IA + SpawnPoint Manager

--- LightCycle/Assets/Scripts/manager.cs
+++ LightCycle/Assets/Scripts/manager.cs
@@ -1,23 +1,24 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Manager : MonoBehaviour
 {
-    public List<SpawnPointEnemy> enemySpawnPoint { get; private set; }
+    public List<SpawnPointEnemy> EnemySpawnPoint { get; private set; }
 
-    private int i;
+    public bool canTheUnitJump = true;
     
     void Start()
     {
-        i = 0;
         GameObject[] spawnObjects = GameObject.FindGameObjectsWithTag("ESP");
-        enemySpawnPoint = new List<SpawnPointEnemy>();
+        EnemySpawnPoint = new List<SpawnPointEnemy>();
 
         for (int i = 0; i < spawnObjects.Length; i++)
         {
-            enemySpawnPoint.Add(spawnObjects[i].GetComponent<SpawnPointEnemy>());
-
-            if (enemySpawnPoint[i] == null)
+            SpawnPointEnemy spe = spawnObjects[i].GetComponent<SpawnPointEnemy>();
+            spe.cantheunitjump = canTheUnitJump;
+            EnemySpawnPoint.Add(spe);
+            if (EnemySpawnPoint[i] == null)
             {
                 Debug.LogWarning("SpawnPointEnemy component not found on object: " + spawnObjects[i].name);
             }
@@ -26,23 +27,43 @@ public class Manager : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        for (int i = 0; i < EnemySpawnPoint.Count; i++)
         {
-            if (0 != enemySpawnPoint.Count)
+            if (EnemySpawnPoint[i].CanSpawn)
             {
-                if (i >= enemySpawnPoint.Count)
+                // ReSharper disable once Unity.PerformanceCriticalCodeInvocation
+                int b = SpawnoneUnitAt(i);
+                if (b == -1)
                 {
-                    i = 0;
+                    EnemySpawnPoint.RemoveAt(i);
+                    i--;
                 }
-
-                bool? b = enemySpawnPoint[i].spawn_ennemy();
-                if(b == true)enemySpawnPoint.RemoveAt(i);
+                else if (b == -3) Destroy(gameObject);
             }
-        }
+        } 
     }
-    
-    void Useless()
+
+    int SpawnoneUnitAt(int i)
     {
-        
+        if (i < 0) i += EnemySpawnPoint.Count;
+        if (i < 0 || i >= EnemySpawnPoint.Count)
+        {
+            Debug.LogError("i is out of range", this);
+            return -2;
+        }
+        if (0 != EnemySpawnPoint.Count)
+        {
+            if (i >= EnemySpawnPoint.Count)
+            {
+                i = 0;
+            }
+            return EnemySpawnPoint[i].spawn_ennemy();
+            
+        }
+        else
+        {
+            Debug.LogWarning("None SpawnPoint detected", this);
+            return -3;
+        }
     }
 }
