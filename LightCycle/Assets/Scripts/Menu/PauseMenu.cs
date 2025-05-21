1f8a009310430b395dd01e8eda9b3268a5fb179a
Biere-comme-la-boisson <manon.biere@gmail.com>
2025-05-21 16:28:39 +0200

    Plus jolie que zach <3 XD

--- LightCycle/Assets/Scripts/Menu/PauseMenu.cs
+++ LightCycle/Assets/Scripts/Menu/PauseMenu.cs
@@ -1,13 +1,22 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class PauseMenu : MonoBehaviour
 {
 
+    public GameObject dialoguePanel1;
     public GameObject pauseMenu;
     public static bool IsPaused;
 
-    public void PauseGame(){
+    void Start()
+    {
+        dialoguePanel1.SetActive(false);
+    }
+    public void PauseGame()
+    {
+        dialoguePanel1.SetActive(true);
         pauseMenu.SetActive(true);
         Time.timeScale = 0f; //Stop le jeu (les updates/animations)
         IsPaused = true;
@@ -16,18 +25,24 @@ public class PauseMenu : MonoBehaviour
         pauseMenu.SetActive(false);
         Time.timeScale = 1f; //Relance le jeu (les updates/animations)
         IsPaused = false;
+        dialoguePanel1.SetActive(false);
     }
 
     public void GoToMenu(){
         Time.timeScale = 1f; //Relance le jeu (les updates/animations)
         pauseMenu.SetActive(false);
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene("MainMenu");
     }
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape)){
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
             GoToMenu();
+        }
+        if(Input.GetKeyDown("p"))
+        {
+            PauseGame();
         }
     }
 }
