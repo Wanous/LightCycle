604decc904b9aa0159b7003d148f1bf2ce4e00ca
zachary.coates <zachary.coates@epita.fr>
2025-05-23 13:15:00 +0100

    fix problem

--- LightCycle/Assets/Scripts/DiscoveryUIManager.cs
+++ LightCycle/Assets/Scripts/DiscoveryUIManager.cs
@@ -20,178 +20,41 @@ public class DiscoveryUIManager : MonoBehaviour
 
     void Start()
     {
-        if (hostButton != null)
-            hostButton.onClick.AddListener(HostGame);
-        if (findButton != null)
-            findButton.onClick.AddListener(FindServers);
-        if (networkDiscovery != null)
-            networkDiscovery.OnServerFound.AddListener(OnDiscoveredServer);
+        hostButton.onClick.AddListener(HostGame);
+        findButton.onClick.AddListener(FindServers);
+
+        networkDiscovery.OnServerFound.AddListener(OnDiscoveredServer);
     }
 
     public void HostGame()
     {
-        if (networkManager != null)
-        {
-            networkManager.StartHost();
-            Debug.Log("Started hosting game");
-        }
-        if (networkDiscovery != null)
-        {
-            networkDiscovery.AdvertiseServer();
-            Debug.Log("Started advertising server");
-        }
+        networkManager.StartHost();
+        networkDiscovery.AdvertiseServer();
     }
 
     public void FindServers()
     {
         discoveredServers.Clear();
-        ClearServerList();
+        foreach (Transform child in serverListParent)
+            Destroy(child.gameObject);
 
-        if (networkDiscovery != null)
-        {
-            networkDiscovery.StartDiscovery();
-            Debug.Log("Started server discovery");
-        }
+        networkDiscovery.StartDiscovery();
     }
 
-    private void ClearServerList()
+    void OnDiscoveredServer(ServerResponse info)
     {
-        if (serverListParent != null)
-        {
-            foreach (Transform child in serverListParent)
-                Destroy(child.gameObject);
-        }
-    }
-
-    public void OnDiscoveredServer(ServerResponse info)
-    {
-        Debug.Log($"Server discovered: {info.serverId}, URI: {info.uri}");
-
         if (discoveredServers.ContainsKey(info.serverId))
-        {
-            Debug.Log($"Server {info.serverId} already in list, skipping");
             return;
-        }
 
         discoveredServers[info.serverId] = info;
-        CreateServerButton(info);
-    }
-
-    private void CreateServerButton(ServerResponse info)
-    {
-        if (serverButtonPrefab == null || serverListParent == null)
-        {
-            Debug.LogError("Server button prefab or parent is null!");
-            return;
-        }
 
         GameObject buttonObj = Instantiate(serverButtonPrefab, serverListParent);
+        buttonObj.GetComponentInChildren<TMP_Text>().text = info.EndPoint.Address.ToString();
 
-        // Try multiple ways to find the text component
-        TMP_Text buttonTMPText = buttonObj.GetComponentInChildren<TMP_Text>(true);
-        if (buttonTMPText == null)
-        {
-            // Try getting Text component instead
-            Text buttonText = buttonObj.GetComponentInChildren<Text>(true);
-            if (buttonText != null)
-            {
-                buttonText.text = GetServerDisplayText(info);
-                buttonText.color = Color.white;
-                buttonText.fontSize = 24;
-            }
-        }
-        else
-        {
-            buttonTMPText.text = GetServerDisplayText(info);
-            buttonTMPText.color = Color.white;
-            buttonTMPText.fontSize = 24;
-            buttonTMPText.enableAutoSizing = true;
-            buttonTMPText.overflowMode = TextOverflowModes.Overflow;
-        }
-
-        Button button = buttonObj.GetComponent<Button>();
-        if (button != null)
-        {
-            // Capture the server info in a local variable to avoid closure issues
-            ServerResponse serverInfo = info;
-            button.onClick.AddListener(() => JoinServer(serverInfo));
-        }
-        else
-        {
-            Debug.LogError("Button component not found on server button prefab!");
-        }
-    }
-
-    private string GetServerDisplayText(ServerResponse info)
-    {
-        // Try different ways to get the IP address
-        string displayText = "Unknown Server";
-
-        if (info.uri != null)
-        {
-            // Option 1: Use Host property
-            if (!string.IsNullOrEmpty(info.uri.Host))
-            {
-                displayText = $"Join: {info.uri.Host}:{info.uri.Port}";
-            }
-            // Option 2: Use the full URI
-            else
-            {
-                displayText = $"Join: {info.uri}";
-            }
-        }
-        // Option 3: Use EndPoint if available
-        else if (info.EndPoint != null)
-        {
-            displayText = $"Join: {info.EndPoint}";
-        }
-
-        Debug.Log($"Server display text: {displayText}");
-        return displayText;
-    }
-
-    private void JoinServer(ServerResponse info)
-    {
-        Debug.Log($"Attempting to join server: {info.uri}");
-
-        if (networkDiscovery != null)
+        buttonObj.GetComponent<Button>().onClick.AddListener(() =>
         {
             networkDiscovery.StopDiscovery();
-        }
-
-        if (networkManager != null)
-        {
-            // Make sure we're not already connected
-            if (networkManager.isNetworkActive)
-            {
-                networkManager.StopClient();
-                networkManager.StopHost();
-            }
-
-            // Start client with the server URI
             networkManager.StartClient(info.uri);
-        }
-        else
-        {
-            Debug.LogError("NetworkManager is null!");
-        }
-    }
-
-    // Optional: Add a method to stop discovery
-    public void StopDiscovery()
-    {
-        if (networkDiscovery != null)
-        {
-            networkDiscovery.StopDiscovery();
-        }
-    }
-
-    // Optional: Clean up on destroy
-    void OnDestroy()
-    {
-        if (networkDiscovery != null)
-        {
-            networkDiscovery.OnServerFound.RemoveListener(OnDiscoveredServer);
-        }
+        });
     }
 }
